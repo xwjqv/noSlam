53198cf240ce864654693be8da0e33239f053693
xwjqv <35635774+xwjqv@users.noreply.github.com>
2018-03-04 20:38:23 +0100

    Anfänge der der Schritterkennung

--- noSlam.ino
+++ noSlam.ino
@@ -1,19 +1,41 @@
-int Sensorrechts=2;
-int Sensorlinks=3;
+#include "Encoder.h"
 
-int Motorrechts=4;
-int Motorlinks=5;
+const int PSR=2; //Pin Sensor Rechts
+const int PSL=3; //Pin Sensor Links
 
-void setup() 
+const int PMR=4; //Pin Motor Rechts
+const int PML=5; //Pin Motor Links
+
+const int pwmR=6;
+const int pwmL=7;
+
+const int ER=8;
+const int EL=9;
+
+const int pwmDuty=0xaf; //x0 bedeutet hexadezimal
+
+void setup()
 {
-pinMode(Sensorrechts, INPUT);
-pinMode(Sensorlinks, INPUT);
+	currentBlock = new DBlock;
 
-attachInterrupt(digitalPinToInterrupt(Sensorlinks), Lenkunglinks, CHANGE);
-attachInterrupt(digitalPinToInterrupt(Sensorrechts), Lenkungrechts, CHANGE);
+	//currentBlock = new DBlock;
 
-pinMode(Motorrechts, OUTPUT);
-pinMode(Motorlinks, OUTPUT);
+	TimerInit();
+
+	pinMode(PSR, INPUT);
+	pinMode(PSL, INPUT);
+	
+	attachInterrupt(digitalPinToInterrupt(PSL), Lenkunglinks, CHANGE);
+	attachInterrupt(digitalPinToInterrupt(PSR), Lenkungrechts, CHANGE);
+	
+	pinMode(PMR, OUTPUT);
+	pinMode(PML, OUTPUT);
+	
+	pinMode(pwmR, OUTPUT);
+	pinMode(pwmL, OUTPUT);
+	
+	analogWrite(pwmR, pwmDuty);
+	analogWrite(pwmL, pwmDuty);
 }
 
 void loop()
@@ -23,20 +45,29 @@ void loop()
 
 void Lenkunglinks()
 {
-	if(digitalRead(Sensorlinks)== LOW){
-		digitalWrite(Motorlinks, HIGH);
+	if(digitalRead(PSL)== LOW){
+		digitalWrite(PML, HIGH);
 	}else{
-		digitalWrite(Motorlinks, LOW);
+		if(digitalRead(PSR)== LOW){
+			digitalWrite(PML, LOW);
+		}else{
+			digitalWrite(PML, HIGH);
+		}
 	}
 
 }
 
 void Lenkungrechts()
 {
-	if(digitalRead(Sensorrechts)== LOW){
-		digitalWrite(Motorrechts, HIGH);
+	if(digitalRead(PSR)== LOW){
+		digitalWrite(PMR, HIGH);
 	}else{
-		digitalWrite(Motorrechts, LOW);
+		if(digitalRead(PSL)== LOW){
+			digitalWrite(PMR, LOW);
+		}else{
+			digitalWrite(PMR, HIGH);
+		}
 	}
 
 }
+
